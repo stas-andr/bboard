d2ab5c004ae3df417d064f69f7b97ad09abebc7d
stasofob <64842680+stasofob@users.noreply.github.com>
2022-04-27 16:25:33 +0300

    создана модель объявлений

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from .utilities import get_timestamp_path
+
 class AdvUser(AbstractUser):
     is_activated = models.BooleanField(default=True, db_index=True,
                                        verbose_name='Прошел активацию?')
@@ -50,3 +52,29 @@ class SubRubric(Rubric):
         ordering = ('super_rubric__order', 'super_rubric__name', 'order', 'name')
         verbose_name = 'Подрубрика'
         verbose_name_plural = 'Подрубрики'
+
+class Bb(models.Model):
+    rubric = models.ForeignKey(SubRubric, on_delete=models.PROTECT,
+                               verbose_name='Рубрика')
+    title = models.CharField(max_length=40, verbose_name='Товар')
+    content = models.TextField(verbose_name='Описание')
+    price = models.FloatField(default=0, verbose_name="Цена")
+    contacts = models.TextField(verbose_name='Контакты')
+    image = models.ImageField(blank=True, upload_to=get_timestamp_path,
+                              verbose_name='Изображение')
+    author = models.ForeignKey(AdvUser, on_delete=models.CASCADE,
+                               verbose_name='Автор объявления')
+    is_active = models.BooleanField(default=True, db_index=True,
+                                    verbose_name='Выводить в списке?')
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True,
+                                      verbose_name="Опубликовано")
+
+    def delete(self, *args, **kwargs):
+        for ai in self.additionalimage_set.all():
+            ai.delete()
+        super().delete(*args, **kwargs)
+
+    class Meta:
+        verbose_name_plural = 'Объявления'
+        verbose_name = 'Объявление'
+        ordering = ['-created_at']
